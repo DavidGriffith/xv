169740b822234b41580692c686d4c96f17e8f799
David Griffith <dave@661.org>
2017-04-03 05:40:41 -0700

    Adding beginnings of autoconf / automake stuff

---
+++ autogen.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Run this to set up the package for configuring.
+
+run()
+{
+    echo "Running '$@'"
+    eval "$@"
+}
+
+echo
+run aclocal && run automake -a -c -f && run autoconf || exit 1
+echo
+echo Now run \'./configure\'
