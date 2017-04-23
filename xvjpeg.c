4264f830476b93046ac6eb871e1ddc7d9cf7b784
David Griffith <dave@661.org>
2017-04-23 11:01:49 -0700

    Apply xv-20090907-patrick-keshishian-CMYK-JPEG-crash-fix.patch.  See #4.
    The provided image file that's supposed to trigger this bug doesn't
    cause trouble on my machine.  Maybe it's a 32-bitness versus 64-bitness
    problem.

--- xvjpeg.c
+++ xvjpeg.c
@@ -696,7 +696,7 @@ L2:
         if ((cmy = *q++ - k) < 0) cmy = 0; *p++ = cmy; /* R */
         if ((cmy = *q++ - k) < 0) cmy = 0; *p++ = cmy; /* G */
         if ((cmy = *q++ - k) < 0) cmy = 0; *p++ = cmy; /* B */
-      } while (++q <= pic_end);
+      } while (++q < pic_end);
     }
     else { /* assume normal data */
       register byte *q = pic;
@@ -707,7 +707,7 @@ L2:
         if ((cmy = k - *q++) < 0) cmy = 0; *p++ = cmy; /* R */
         if ((cmy = k - *q++) < 0) cmy = 0; *p++ = cmy; /* G */
         if ((cmy = k - *q++) < 0) cmy = 0; *p++ = cmy; /* B */
-      } while (++q <= pic_end);
+      } while (++q < pic_end);
     }
     pic = realloc(pic,p-pic); /* Release extra storage */
   }
